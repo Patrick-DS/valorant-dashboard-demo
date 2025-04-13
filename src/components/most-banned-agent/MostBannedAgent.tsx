d125b5687514c3785f3ad124551997cf73710310
Patrick Da Silva <patrick1dasilva@gmail.com>
2025-04-13 17:13:15 +0300

    Created basic layout according to design

--- src/components/most-banned-agent/MostBannedAgent.tsx
+++ src/components/most-banned-agent/MostBannedAgent.tsx
@@ -1,5 +1,11 @@
 import "./MostBannedAgent.scss"
 
-const MostBannedAgent = () => <div className="container" />
+const MostBannedAgent = () => (
+    <div className="most-banned-agent-container">
+        <h3>Most Banned Agent</h3>
+        <span>Something</span>
+        <span>Something</span>
+    </div>
+)
 
 export default MostBannedAgent
--- src/components/most-picked-agent/MostPickedAgent.tsx
+++ src/components/most-picked-agent/MostPickedAgent.tsx
@@ -1,5 +1,11 @@
 import "./MostPickedAgent.scss"
 
-const MostPickedAgent = () => <div className="container" />
+const MostPickedAgent = () => (
+    <div className="most-picked-agent-container">
+        <h3>Most Banned Agent</h3>
+        <span>Something</span>
+        <span>Something</span>
+    </div>
+)
 
 export default MostPickedAgent
