9769f6f705a3d7a06d1dc584145bd8b19690cf62
Patrick Da Silva <patrick1dasilva@gmail.com>
2025-04-13 17:27:15 +0300

    Created content for the most banned/picked agents

--- src/components/most-banned-agent/MostBannedAgent.tsx
+++ src/components/most-banned-agent/MostBannedAgent.tsx
@@ -3,8 +3,10 @@ import "./MostBannedAgent.scss"
 const MostBannedAgent = () => (
     <div className="most-banned-agent-container">
         <h3>Most Banned Agent</h3>
-        <span>Something</span>
-        <span>Something</span>
+        <div className="most-banned-agent-details">
+            <span className="ban-count">Tejo: 170 bans</span>
+            <span className="ban-peak">Peak: 1st Ban (66)</span>
+        </div>
     </div>
 )
 
--- src/components/most-picked-agent/MostPickedAgent.tsx
+++ src/components/most-picked-agent/MostPickedAgent.tsx
@@ -2,9 +2,11 @@ import "./MostPickedAgent.scss"
 
 const MostPickedAgent = () => (
     <div className="most-picked-agent-container">
-        <h3>Most Banned Agent</h3>
-        <span>Something</span>
-        <span>Something</span>
+        <h3>Most Picked Agent</h3>
+        <div className="most-picked-agent-details">
+            <span className="ban-count">Omen: 223 picks</span>
+            <span className="ban-peak">Peak: 3rd Pick (48)</span>
+        </div>
     </div>
 )
 
