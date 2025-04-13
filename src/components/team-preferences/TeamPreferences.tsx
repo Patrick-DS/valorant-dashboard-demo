6cc8157f3b584e8f1e61eec924931622b02b6b0a
Patrick Da Silva <patrick1dasilva@gmail.com>
2025-04-13 17:41:43 +0300

    Created content for the Team Preferences panel

--- src/components/team-preferences/TeamPreferences.tsx
+++ src/components/team-preferences/TeamPreferences.tsx
@@ -1,5 +1,26 @@
 import "./TeamPreferences.scss"
 
-const TeamPreferences = () => <section className="team-preferences-container" />
+const MostXedByTeamsList = () => <div className="list-entry-container">
+    {[1,2,3,4,5].map(() => (
+        <div className="list-entry">
+            <span className="list-entry-agent">Tejo</span>
+            <span className="list-entry-team">arise(9)</span>
+        </div>
+    ))}
+</div>
+
+const TeamPreferences = () => (
+    <section className="team-preferences-container">
+        <h3>Team Preferences</h3>
+        <div>
+            <h4>Most Banned By Teams</h4>
+            <MostXedByTeamsList />
+        </div>
+        <div>
+            <h4>Most Picked By Teams</h4>
+            <MostXedByTeamsList />
+        </div>
+    </section>
+)
 
 export default TeamPreferences
